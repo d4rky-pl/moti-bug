2b6c363d967c593bd35d4cddae877b637f85531a
Michał Matyas <github@higher.lv>
2023-05-14 19:16:06 +0200

    Add code

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { MotiView } from 'moti'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MotiView
+      from={{ scale: 0 }}
+      animate={{ scale: [0, 100] }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,10 @@
+#### Minimal repro of https://github.com/nandorojo/moti/issues/282
+
+How to run:
+
+```
+yarn
+yarn start -c -i
+```
+
+Issue confirmed on both iOS (Simulator) and Android (real device)
